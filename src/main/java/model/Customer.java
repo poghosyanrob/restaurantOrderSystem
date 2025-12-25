6375a0309b2cf86d12de0237b6f1d4bb49a5de77
Robert Poghosyan <robert.poghosyan.g@gmail.com>
2025-12-25 13:50:51 +0400

    all is ok

--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -10,6 +10,6 @@ import lombok.NoArgsConstructor;
 public class Customer {
     private int id;
     private String name;
-    private String phone;
+    private String  phone;
     private String email;
 }
